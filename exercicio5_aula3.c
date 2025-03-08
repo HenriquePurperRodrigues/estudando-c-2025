787de1762d8bd36aa7da07ba64c05adfbc177cb2
Henrique <henriquepurper54@gmail.com>
2025-03-07 21:17:36 -0300

    Update exercicio5_aula3.c

--- exercicio5_aula3.c
+++ exercicio5_aula3.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+//Faça um algoritmo que calcula os gastos com combustível em uma viagem. O
+//algoritmo deve solicitar ao usuário a distância a ser percorrida em Km, o consumo do
+//carro em Km/litro e o preço do litro do combustível. Como resposta o programa deverá
+//informar qual o valor em R$ a ser gasto com combustível na viagem.
 
 int main ()
 {
